d572413133501aa99685c1509318219e8ce88ccf
Johan Lindberg <johan.lindberg@bzzt.se>
2019-11-13 14:01:30 +0100

    Added a game board representation.

--- doodle.py
+++ doodle.py
@@ -2,12 +2,35 @@
 
 import curses
 
+__BOARD__ = [['*','*','*','*','*','*','*',],
+             ['*','*','*','*','*','*','*',],
+             ['*','*','*','*','*','*','*',],
+             ['*','*','*','*','*','*','*',],
+             ['*','*','*','*','*','*','*',]]
+
 def game(stdscr):
+    view(stdscr, __BOARD__)
+
+def view(stdscr, board):
+    # hide cursor
+    curses.curs_set(0)
+
+    # output the board on the screen
+    y = 0
+    for row in board:
+        x = 0
+        for col in row:
+            stdscr.addch(y,x, col)
+            x += 1
+        y += 1
+
+    stdscr.refresh()
+
     # wait for user to press <ESC>
     while True:
         key = stdscr.getch()
         if key == 27: # 27 is the <ESC> ASCII code
-            break
+            raise Exception("%d %d" % (y, x))
 
 if __name__ == "__main__":
     curses.wrapper(game)
